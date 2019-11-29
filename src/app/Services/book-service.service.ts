4a3b771a7fd628e14ce777b02bd2f089c1fef7b8
unknown <G00346183@gmit.ie>
2019-11-29 10:29:01 +0000

    second coimmit

--- src/app/Services/book-service.service.ts
+++ src/app/Services/book-service.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Movie} from '../book.model';
+import {Book} from '../book.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MovieServiceService {
+export class BookServiceService {
 
   constructor(private http:HttpClient) { }
 
-  GetMovieInformation():Observable<any>{
+  GetBookInformation():Observable<any>{
     return this.http.get('http://localhost:4000/api/books');
   }
 
-  AddMovieInformation(title:string,year:string,author:string):Observable<any>{
-    const book:Movie = {title:title, year:year, author:author};
+  AddBookInformation(title:string,year:string,author:string):Observable<any>{
+    const book:Book = {title:title, year:year, author:author};
     return this.http.post('http://localhost:4000/api/books', book)
   }
 
-  DeleteMovie(id:String):Observable<any>{
+  DeleteBook(id:String):Observable<any>{
     return this.http.delete('http://localhost:4000/api/books/'+id);
   }
 
-  GetMovie(id:String):Observable<any>{
+  GetBook(id:String):Observable<any>{
     return this.http.get('http://localhost:4000/api/books/'+id);
   }
 
-  UpdateMovie(id:String,title:string, year:string, author:string):Observable<any>{
-    const book:Movie = {title:title, year:year, author:author};
+  UpdateBook(id:String,title:string, year:string, author:string):Observable<any>{
+    const book:Book = {title:title, year:year, author:author};
     console.log("Edit"+id);
     return this.http.put('http://localhost:4000/api/books/'+id, book);
   }
