90ca813ea5adebb97e22d601d6e8f6eb57795da8
unknown <G00346183@gmit.ie>
2019-12-01 17:57:39 +0000

    comment here

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,29 +1,37 @@
 // server.js
 
 const express = require('express');
+//Necessary to load html files
 const app = express();
+//Needed for interpreting post requests
 const bodyParser = require('body-parser');
+//Needed for Cross Origin Resource Sharing(CORS)
 const PORT = 4000;
+//Needed to connect to the mongo db
 const cors = require('cors');
+//Needed for interpreting post requests
 const mongoose = require('mongoose');
-
+//Connection string to mongodb
 const mongoDB = 'mongodb+srv://Conor:mongodb123@cluster0-ndyqk.mongodb.net/test?retryWrites=true&w=majority'
+//Connect to database
 mongoose.connect(mongoDB, {useNewUrlParser:true});
-
+//Needed for body parssing
 const Schema = mongoose.Schema;
+//Get reference to schema above
 
+//Create new schema for books
 const bookSchema = new Schema({
   title:String,
   year:String,
   author:String
 });
-
+//Create model for the database
 const BookModel = mongoose.model('book',bookSchema);
-
+//Use Cross reference Origin system
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+//Helps to avoid CORS errors
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -36,15 +44,15 @@ app.use(function(req, res, next) {
 app.get('/', (req, res) => {
   res.send('hello world');
 })
-
+//Get the list of books from the mongo database
 app.get('/api/books', (req,res,next) => {
-
+//Retrieve the data from the books model
   console.log("get request")
   BookModel.find((err,data)=>{
     res.json({books:data});
   })
 })
-
+//delete model for the book
 app.delete('/api/books/:id', (req,res) =>{
   console.log(req.params.id);
 
@@ -55,7 +63,7 @@ app.delete('/api/books/:id', (req,res) =>{
     res.json(data);
   })
 })
-
+//displays the books
 app.get('/api/books/search/:title/:criteria', (req,res)=>{
   console.log(req.params.title);
   console.log(req.params.criteria);
